1a9cee32094c7de4d7f261c6c1c7eecc445b0ea2
Erez <erezmirav@gmail.com>
2024-07-11 15:44:53 +0300

    fixed smooth scrolling

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="!scroll-smooth">
       <body className={JetBrainsMono.className}>{children}</body>
     </html>
   );
